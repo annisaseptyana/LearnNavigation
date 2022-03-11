25f368d18b3928e7bb36cbeb002e33ca577944ff
annisaseptyana <starrynyteskye@gmail.com>
2022-03-11 15:39:42 +0700

    Action Bar - Option Menu

--- app/src/main/java/com/bangkit/learnnavigation/ActionBar.kt
+++ app/src/main/java/com/bangkit/learnnavigation/ActionBar.kt
@@ -1,11 +1,46 @@
 package com.bangkit.learnnavigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.bangkit.learnnavigation.databinding.ActivityActionBarBinding
 
 class ActionBar : AppCompatActivity() {
+
+    private lateinit var binding: ActivityActionBarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_action_bar)
+        binding = ActivityActionBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    // Menu -> public class?
+    // Menampilkan menu.xml
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    // Listener untuk menu ActionBar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+            }
+            R.id.menu2 -> {
+                val i = Intent(this, MenuActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
     }
 }
